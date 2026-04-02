e91968116200df2cb3e1f908aa5b128e26b093f5
HP-Probook <emmanuel19727@gmail.com>
2026-04-02 17:02:44 +0300

    ~

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -174,7 +174,7 @@ export default function HomePage() {
       {/* Hero Section */}
       <section className="">
         <div className="max-w-7xl mx-auto p-1.5 md:p-4">
-          <div className="flex gap-4">
+          <div className="flex gap-2">
             {/* Left Column - Categories */}
             <div className="hidden lg:block w-56 shrink-0">
               <div className="bg-white rounded-sm shadow-sm shadow-ig-green-light overflow-hidden h-full">
